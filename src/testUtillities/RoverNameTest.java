b0e6ae47d568e9a6912a89bc9874b3247567256b
Dipesh <dipesh.290193@gmail.com>
2016-04-18 18:12:57 -0700

    Added Test case

--- src/testUtillities/RoverNameTest.java
+++ src/testUtillities/RoverNameTest.java
@@ -2,20 +2,24 @@ package testUtillities;
 
 import common.Rover;
 import enums.RoverName;
+import org.junit.*;
 
 public class RoverNameTest {
 	public static void main(String[] args) {
 		System.out.println("test roverName test running");
 		
-		String name = "ROVER_00";
-				
+		String name = "ROVER_08";
         RoverName rname = RoverName.getEnum(name); 
-        
         System.out.println("SWARM: make a rover name " + rname);
-        
-        //Rover rover = new Rover(rname);
-        
+        new RoverNameTest().RoverNameTestMethod();
         
         
+	}
+	
+	@Test
+	public void RoverNameTestMethod()
+	{
+			String name = "ROVER_08";
+		  assert name.equals(RoverName.getEnum(name).toString());
 	}
 }
